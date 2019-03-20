f99f0e4cdf71d0f9784667c619d248765d20139d
Benjamin Lannon <benjamin@lannonbr.com>
2019-03-20 19:16:42 -0400

    Adding meetup.com link at bottom of group pages
    For Hack Upstate, I added a custom link to their website.
    
    In the future, this will be removed when each page has upcoming / past events on
    each particular group page
    
    Closes #74

--- src/content/groups/hack_upstate.md
+++ src/content/groups/hack_upstate.md
@@ -14,3 +14,5 @@ groupType: hackathon
 Advancing Upstate New York’s tech community through events and education.
 
 Hack Upstate organizes weekend hackathons twice a year (i.e., Fall & Spring) where developers, designers, engineers and innovators from across Upstate New York come to Syracuse, NY, to share ideas, form teams, build projects, and win awesome prizes in 24 hours. The event is free to attend for all participants. Join us!
+
+If you want to learn more, visit <a href="https://hackupstate.com/">hackupstate.com</a>
--- src/templates/mdxTemplate.js
+++ src/templates/mdxTemplate.js
@@ -6,6 +6,16 @@ import Layout from '../components/layout'
 const MDXTemplate = ({ data }) => (
   <Layout frontmatter={data.mdx.frontmatter}>
     <MDXRenderer>{data.mdx.code.body}</MDXRenderer>
+    {data.mdx.frontmatter.groupType &&
+      data.mdx.frontmatter.title !== `Hack Upstate` && (
+        <p>
+          If you want to see upcoming meetups for {data.mdx.frontmatter.title}{' '}
+          or any of the other groups, visit the{' '}
+          <a href="https://www.meetup.com/Syracuse-Software-Development-Meetup/">
+            Meetup.com page
+          </a>
+        </p>
+      )}
   </Layout>
 )
 
@@ -17,6 +27,7 @@ export const PageQuey = graphql`
       }
       frontmatter {
         title
+        groupType
         subtitle
         wide
         imgAlt
