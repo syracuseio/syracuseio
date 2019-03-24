129f7e07a247a45a136551b576f6a2392275c119
Benjamin Lannon <benjamin@lannonbr.com>
2019-03-24 11:05:48 -0400

    Clean up Upcoming Meetups some more

---
+++ src/components/EventPreviewCard.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import moment from 'moment'
+import styled from 'styled-components'
+
+const EventContainer = styled.div`
+  margin-top: 20px;
+
+  a {
+    color: #008aff;
+    text-decoration: none;
+  }
+
+  blockquote {
+    border-left: 5px solid #eee;
+    padding-left: 26px;
+    margin-left: 0;
+    color: #808080;
+    font-style: italic;
+  }
+`
+
+/**
+ * Parse out first paragraph of event description
+ * @param {string} desc - html string recieved from Meetup API
+ */
+function parseDescription(desc) {
+  // If desc is not html, pass it through
+  if (desc[0] !== '<') return desc
+
+  let findStr = "What we'll do<br/>"
+
+  let idx = desc.indexOf(findStr)
+
+  if (idx === -1) {
+    findStr = '<p>'
+    idx = desc.indexOf(findStr)
+  }
+
+  let idxEnd = desc.indexOf('</p>', idx)
+
+  return desc.slice(idx + findStr.length, idxEnd)
+}
+
+function EventPreviewCard(props) {
+  let { description, local_date, link, name } = props.meetup
+
+  description = parseDescription(description)
+  let dateMomentObj = moment(local_date)
+
+  let fullDateString = dateMomentObj.format('dddd, MMMM Do, YYYY')
+  let timeFromNow = dateMomentObj.fromNow()
+
+  return (
+    <EventContainer>
+      <strong>
+        <time>{fullDateString}</time>{' '}
+        <span style={{ color: '#646464', fontWeight: '500' }}>
+          ({timeFromNow})
+        </span>
+      </strong>
+      <h4 style={{ marginTop: 0 }}>
+        <a href={link}>{name}</a>
+      </h4>
+      <blockquote>
+        <p dangerouslySetInnerHTML={{ __html: description }} />
+      </blockquote>
+      {props.idx !== props.numMeetups - 1 && <hr />}
+    </EventContainer>
+  )
+}
+
+export default EventPreviewCard
--- src/components/UpcomingMeetups.js
+++ src/components/UpcomingMeetups.js
@@ -1,24 +1,7 @@
 import React from 'react'
-import { StaticQuery, graphql } from 'gatsby'
+import { useStaticQuery, graphql } from 'gatsby'
 import moment from 'moment'
-import styled from 'styled-components'
-
-const EventContainer = styled.div`
-  margin-top: 20px;
-
-  a {
-    color: #008aff;
-    text-decoration: none;
-  }
-
-  blockquote {
-    border-left: 5px solid #eee;
-    padding-left: 26px;
-    margin-left: 0;
-    color: #808080;
-    font-style: italic;
-  }
-`
+import EventPreviewCard from './EventPreviewCard'
 
 const query = graphql`
   {
@@ -40,72 +23,29 @@ const query = graphql`
   }
 `
 
-/**
- * Parse out first paragraph of event description
- * @param {string} desc - html string recieved from Meetup API
- */
-function parseDescription(desc) {
-  // If desc is not html, pass it through
-  if (desc[0] !== '<') return desc
-
-  let findStr = "What we'll do<br/>"
-
-  let idx = desc.indexOf(findStr)
-
-  if (idx === -1) {
-    findStr = '<p>'
-    idx = desc.indexOf(findStr)
-  }
-
-  let idxEnd = desc.indexOf('</p>', idx)
-
-  return desc.slice(idx + findStr.length, idxEnd)
-}
-
 function UpcomingMeetups() {
+  let data = useStaticQuery(query)
+
+  let meetups = data.allSyrEvent.edges
+  let sixWeeksMoment = moment().add(6, 'weeks')
+
+  meetups = meetups.filter(evt => {
+    let evtDate = moment(evt.node.local_date)
+    return sixWeeksMoment.diff(evtDate) >= 0
+  })
+
   return (
-    <StaticQuery
-      query={query}
-      render={data => {
-        let meetups = data.allSyrEvent.edges
-        let sixWeeksMoment = moment().add(6, 'weeks')
-
-        meetups = meetups.filter(evt => {
-          let evtDate = moment(evt.node.local_date, 'YYYY-MM-DD')
-          return sixWeeksMoment.diff(evtDate) >= 0
-        })
-
-        return (
-          <>
-            <h2>Upcoming developer events</h2>
-            {meetups.map(({ node }, idx) => {
-              let description = parseDescription(node.description)
-              let dateMomentObj = moment(node.local_date, 'YYYY-MM-DD')
-
-              let parsedDate = dateMomentObj.format('dddd, MMMM Do, YYYY')
-
-              return (
-                <EventContainer key={node.id}>
-                  <strong>
-                    <time>{parsedDate}</time>{' '}
-                    <span style={{ color: '#646464', fontWeight: '500' }}>
-                      ({dateMomentObj.fromNow()})
-                    </span>
-                  </strong>
-                  <h4 style={{ marginTop: 0 }}>
-                    <a href={node.link}>{node.name}</a>
-                  </h4>
-                  <blockquote>
-                    <p dangerouslySetInnerHTML={{ __html: description }} />
-                  </blockquote>
-                  {idx !== meetups.length - 1 && <hr />}
-                </EventContainer>
-              )
-            })}
-          </>
-        )
-      }}
-    />
+    <>
+      <h2>Upcoming developer events</h2>
+      {meetups.map(({ node }, idx) => (
+        <EventPreviewCard
+          meetup={node}
+          idx={idx}
+          numMeetups={meetups.length}
+          key={node.id}
+        />
+      ))}
+    </>
   )
 }
 
